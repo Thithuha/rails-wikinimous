6ba5f41fd982180f3dbab314bf5a03e930fc8890
thithuha nguyen <chloe.nguyen1910@gmail.com>
2021-08-15 21:20:10 +0200

    adding Faker gem to create 10 random articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :articles
+
+  # #Read all articles
+  # get 'articles' => 'articles#index'
+
+  # #Read an article
+  # get 'articles/:id' => 'articles#show'
+
+  # #Create an article
+  # get 'articles/new' => 'articles#new'
+  # post 'articles' => 'articles#create'
+
+  # #Update an article
+  # get 'articles/:id/edit' => 'articles#edit'
+  # patch 'articles/:id' => 'articles#update'
+
+  # #Delete an article
+  # delete 'articles/:id' => 'articles#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# create 10 tickets in DB
+10.times do
+  Article.create(
+    title: Faker::Coffee.blend_name,
+    content: Faker::JapaneseMedia::StudioGhibli.quote  # generate a fake paragraph
+    )
+end
